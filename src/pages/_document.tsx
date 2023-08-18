2687dee5158f858d17226babe1675e5164740090
Toxic_Blade <toxicblade.work@gmail.com>
2023-08-18 14:27:40 +0300

    .

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html data-theme="winter">
+    <Html data-theme="light">
       <Head />
       <body>
         <Main />
